8cce244124a894e6a1b87b8fa2d4502e222ff3d5
Eric Bossler <eric.bossler@gmail.com>
2020-05-02 10:11:28 -0700

    updated schema to break out between different files

---
+++ models/rounds.js
@@ -0,0 +1,25 @@
+const mongoose = require(mongoose);
+const Schema = mongoose.Schema;
+
+
+const roundSchema = new Schema (
+    {
+        golfer:  {
+            type: String,
+            trim: true,
+            required: "Which golfer is golfing?"
+        },
+        matchId: {
+            type: String,
+            // reference the mongoose id for each match
+            required: "What is the Match ID"
+        },
+        score: {
+            type: Array
+        }
+    }
+)
+
+const Round = mongoose.model("Round", roundSchema)
+
+module.exports = Round;
