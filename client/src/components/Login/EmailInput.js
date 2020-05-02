3d98c0f3e5632398451434a71e7686080fffcfe4
Austen Turner <austenpturner@msn.com>
2020-05-02 13:55:10 -0700

    change html attributes to react attributes

--- client/src/components/Login/EmailInput.js
+++ client/src/components/Login/EmailInput.js
@@ -3,7 +3,7 @@ import React from "react";
 function EmailInput() {
     return(
         <div className="form-group">
-            <label for="email-input">Email address</label>
+            <label htmlFor="email-input">Email address</label>
             <input type="email" className="form-control" id="email-input" name='email' placeholder="Email"></input>
         </div>
     )
