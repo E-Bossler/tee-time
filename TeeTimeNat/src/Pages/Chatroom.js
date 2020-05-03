45313b6c1c02be18b1feb9780ff0d632f23246b5
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-05-03 16:12:25 -0700

    almost have the web client and native client chats talking - making real progress. had to burn down TeeTimeNative file and make new TeeTimeNat file

---
+++ TeeTimeNat/src/Pages/Chatroom.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  // TouchableHighlight,
+} from 'react-native';
+import io from 'socket.io-client';
+export default class Chatroom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatMessage: '',
+      chatMessages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.socket = io('http://192.168.138.2:7777');
+    this.socket.on('connect', () => console.log('connected'));
+    this.socket.on('chat message', msg => {
+      this.setState({chatMessages: [...this.state.chatMessages, msg]});
+    });
+  }
+
+  submitChatMessage() {
+    this.socket.emit('chat message', this.state.chatMessage);
+    this.setState({chatMessage: ''});
+  }
+
+  render() {
+    const chatMessages = this.state.chatMessages.map(chatMessage => (
+      <Text key={chatMessage}>{chatMessage}</Text>
+    ));
+
+    return (
+      <View style={styles.container}>
+        {chatMessages}
+        <TextInput
+          style={{height: 40, borderWidth: 2, top: 600}}
+          autoCorrect={false}
+          value={this.state.chatMessage}
+          onSubmitEditing={() => this.submitChatMessage()}
+          onChangeText={chatMessage => {
+            this.setState({chatMessage});
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 400,
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+});
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const mongoose = require("mongoose");
 const routerApi = require("./routes/routes-api");
 const PORT = process.env.PORT || 7777;
-var server = app.listen(PORT, () =>
-  console.log(`Welcome to port ${PORT}! You are going to rock your day!`)
-);
+var server = http.createServer(app);
 const io = require("socket.io")(server);
 
 app.use(express.urlencoded({ extended: true }));
@@ -35,10 +34,23 @@ app.get("/", (req, res) => {
 
 app.use("/api", routerApi);
 
+server.listen(PORT, () =>
+  console.log(`Welcome to port ${PORT}! You are going to rock your day!`)
+);
+
 io.on("connection", socket => {
   console.log("a user connected :D");
   socket.on("chat message", msg => {
     console.log(msg);
+    // Emits to all clients
     io.emit("chat message", msg);
+  });
+
+  // Everybody but client connecting
+  socket.broadcast.emit("message", "User has joined the chat.");
+
+  //Runs at client disconnect
+  socket.on("disconnect", () => {
+    io.emit("message", "A User has left the chat.");
   });
 });
