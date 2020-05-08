4d0c33614623eff9626234985a1a11a9edd0e4ef
Austen Turner <austenpturner@msn.com>
2020-05-07 22:05:02 -0700

    add dashboard sub-routes for match form and match view

--- Frontend/client/src/components/Dashboard/Main/Main.js
+++ Frontend/client/src/components/Dashboard/Main/Main.js
@@ -1,37 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import NewMatchBtn from "./MatchForm/NewMatchBtn";
 import FormContainer from "./MatchForm/FormContainer";
 import MatchView from "./MatchView/MatchView";
 
 class Main extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            switchView: false
-        };
-    };
-
-    switchView() {
-        console.log("hi");
-        if (this.state.switchView) {
-            this.setState({ switchView: false });
-        } else {
-            this.setState({ switchView: true });
-        }
-    }
-
     render() {
         return(
             <div>
-                <FormContainer 
-                    // passing down switchView function
-                    action={this.switchView.bind(this)}
-                    // passing down startMatch state
-                    hideForm={this.state.switchView} 
-                />
-                <MatchView 
-                    // show match view if state switchView is true
-                    className={this.state.switchView ? "showMatchView" : "hideMatchView"} 
-                />
+                <Route exact path="/dashboard">
+                    <NewMatchBtn />
+                </Route>
+
+                <Route path="/dashboard/matchForm">
+                    <FormContainer />
+                </Route>
+
+                <Route path="/dashboard/matchView">
+                    <MatchView />
+                </Route>
             </div>
         );
     };
--- Frontend/client/src/pages/Dashboard.js
+++ Frontend/client/src/pages/Dashboard.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Nav from '../../../client/src/components/Dashboard/Nav/Nav';
 import Main from '../components/Dashboard/Main/Main';
 
 function Dashboard() {
   return (
     <div>
-      <Nav />
-      <Main />
+      <Router>
+        <Nav />
+        <Switch>
+          <Main />
+        </Switch>
+      </Router>
     </div>
   );
 }
