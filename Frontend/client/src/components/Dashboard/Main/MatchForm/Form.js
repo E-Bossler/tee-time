85a20349a877ec0ad16be8920d0d7b1bd9d362fa
Austen Turner <austenpturner@msn.com>
2020-05-09 13:51:43 -0700

    update friends input and render friends list

--- Frontend/client/src/components/Dashboard/Main/MatchForm/Form.js
+++ Frontend/client/src/components/Dashboard/Main/MatchForm/Form.js
@@ -1,17 +1,63 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
 import CourseInput from "./CourseInput";
-import ParInput from "./ParInput";
+// import ParInput from "./ParInput";
 import FriendsInput from "./FriendsInput";
+import FriendsList from "./FriendsList";
 import "./stylesheet.css";
 
+const findUser = name => {
+    fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(name)
+    }).then(res => {
+      res.json();
+    });
+};
+
 class Form extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            friend: "",
+            friends: []
+        }
+    }
+
+    handleInputChange(event) {
+        let value = event.target.value;
+        this.setState({ friend: value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const friends = this.state.friends;
+        friends.push(this.state.friend);
+        this.setState({ friends: friends });
+
+        console.log(this.state.friend);
+        console.log(this.state.friends);
+
+        findUser(this.state.friend);
+
+        this.setState({ friend: "" });
+    }
+
     render() {
         return(
             <div id="form">
                 <CourseInput />
-                <ParInput />
-                <FriendsInput />
+                <FriendsInput 
+                    handleSubmit={this.handleSubmit.bind(this)} 
+                    handleInputChange={this.handleInputChange.bind(this)} 
+                    friend={this.state.friend}
+                />
+                <FriendsList 
+                    friends={this.state.friends}
+                />
                 <button id="start-match-btn">
                     <Link id="match-link" to="/dashboard/matchView">
                         Start Game
--- Frontend/client/src/components/Dashboard/Main/MatchForm/FriendsInput.js
+++ Frontend/client/src/components/Dashboard/Main/MatchForm/FriendsInput.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import "./stylesheet.css";
 
-function FriendsInput() {
-    return(
-        <div className="form-group">
-            <label htmlFor="friends-input">Friends:</label>
-            <input type="friends" className="form-control" id="friends-input" name='friends'></input>
-        </div>
-    );
+class FriendsInput extends Component {
+    render() {
+        return(
+            <div className="form-group">
+                <label 
+                    htmlFor="friends-input">
+                    Find Friends:
+                </label>
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    id="friends-input" 
+                    name='friend'
+                    defaultValue={this.props.friend}
+                    onChange={this.props.handleInputChange}
+                >
+                </input>
+                <button 
+                    id="add-friend-btn" 
+                    onClick={this.props.handleSubmit}>
+                    Add Friend
+                </button>
+            </div>
+        );
+    }
 };
 
 export default FriendsInput;
