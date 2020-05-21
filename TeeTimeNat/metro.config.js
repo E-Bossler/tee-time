02fffee08f15ae5a7521a45587f314b9e1117044
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-05-21 13:12:36 -0700

    dont know what I broke, but I fixed it. Starting migration and crossing fingers

--- TeeTimeNat/metro.config.js
+++ TeeTimeNat/metro.config.js
@@ -4,7 +4,6 @@
  *
  * @format
  */
-const {getDefaultConfig} = require('metro-config');
 
 module.exports = {
   transformer: {
@@ -16,17 +15,3 @@ module.exports = {
     }),
   },
 };
-
-module.exports = (async () => {
-  const {
-    resolver: {sourceExts},
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-sass-transformer'),
-    },
-    resolver: {
-      sourceExts: [...sourceExts, 'scss', 'sass'],
-    },
-  };
-})();
